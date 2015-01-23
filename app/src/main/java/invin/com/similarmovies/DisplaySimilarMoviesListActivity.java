47c8c1df546b864b014154c08768c68e45718576
Neil Pathare <neil.pathare@outlook.com>
2015-01-23 01:06:28 -0600

    Fixed a bug when if there were no movies found, setting the moviesList to the ListView adapter causes a NullPointerException by adding a flag to set the ListVIew only when moviesList is populated

--- app/src/main/java/invin/com/similarmovies/DisplaySimilarMoviesListActivity.java
+++ app/src/main/java/invin/com/similarmovies/DisplaySimilarMoviesListActivity.java
@@ -16,6 +16,7 @@ import android.widget.ListView;
 import android.widget.SimpleAdapter;
 import android.widget.Toast;
 
+import org.apache.commons.io.IOUtils;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.HttpClient;
@@ -45,21 +46,19 @@ import java.util.zip.GZIPInputStream;
 public class DisplaySimilarMoviesListActivity extends ListActivity{
 
     //'Movies' JSONArray
-    JSONArray moviesJSON;
+    JSONArray moviesJSON = null;
 
     //Hashmap for the ListView
     ArrayList<HashMap<String, String>> movieList;
 
     //String URL helpers
-    private String apiURLPrepender = "http://api.rottentomatoes.com/api/public/v1.0/movies";
-    private String apiSimilarConnector = "similar.json?apikey=";
+    private final String apiURLPrepender = "http://api.rottentomatoes.com/api/public/v1.0/movies";
+    private final String apiSimilarConnector = "similar.json?apikey=";
 
-    private String errorMessage = "";
-    private String forwardSlash = "/";
+    private final String errorMessage = "";
+    private final String forwardSlash = "/";
 
-    public DisplaySimilarMoviesListActivity() {
-        moviesJSON = null;
-    }
+    private boolean isMoviesListNull = true;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -92,10 +91,6 @@ public class DisplaySimilarMoviesListActivity extends ListActivity{
         String apiKey = intent.getStringExtra(HomeScreenActivity.INTENT_KEY);
 
         String similarMoviesJSON = returnSimilarMoviesJSON(movieSearchResultID.trim(), apiKey);
-
-        System.out.println("movieSearchResultID:"+movieSearchResultID);
-        System.out.println("similarMoviesJSON:"+similarMoviesJSON);
-        System.out.println("apiKey:"+apiKey);
 
         if (similarMoviesJSON != null) {
             try {
@@ -130,26 +125,31 @@ public class DisplaySimilarMoviesListActivity extends ListActivity{
                         //Adding the movie to the movie list
                         movieList.add(movieHashMap);
                     }
+                    isMoviesListNull = false;
                 }
             } catch (JSONException e) {
                 //TODO: Implement better error handling
                 errorMessage.concat(":Err:JSONException:");
+                //TODO: Implement logging
             }
         } else {
+            //TODO: Implement better error handling
             errorMessage.concat(":Err:NoData:");
+            //TODO: Implement logging
         }
 
         /**
-         * Updating the parsed JSON data into our ListView
-         * */
-        ListAdapter adapter = new SimpleAdapter(
-                DisplaySimilarMoviesListActivity.this,
-                movieList,
-                R.layout.movies_list,
-                new String[] { HomeScreenActivity.TAG_TITLE },
-                new int[] { R.id.movieTitle });
-
-        setListAdapter(adapter);
+         * If movieList is not null, we should update the parsed JSON data into the ListView
+         */
+        if(!(isMoviesListNull)){
+            ListAdapter adapter = new SimpleAdapter(
+                    DisplaySimilarMoviesListActivity.this,
+                    movieList,
+                    R.layout.movies_list,
+                    new String[] { HomeScreenActivity.TAG_TITLE },
+                    new int[] { R.id.movieTitle });
+            setListAdapter(adapter);
+        }
     }
 
     @Override
@@ -189,35 +189,31 @@ public class DisplaySimilarMoviesListActivity extends ListActivity{
         apiURLBuilder.append(apiSimilarConnector);
         apiURLBuilder.append(apiKey);
 
-        InputStream inputStreamFromAPIResponse = null;
-
         HttpGet similarMoviesHTTPRequest = new HttpGet(apiURLBuilder.toString());
-        System.out.println("similarMoviesHTTPRequest:"+apiURLBuilder.toString());
-
         HttpResponse similarMoviesHTTPResponse;
+
+        InputStream inputStreamFromAPIResponse = null;
+        BufferedReader inputStreamBufferedReader = null;
+        InputStreamReader apiResponseInputStreamReader = null;
+
         try {
             similarMoviesHTTPResponse = defaultHTTPClient.execute(similarMoviesHTTPRequest);
             if(similarMoviesHTTPResponse.equals(null)){
                 apiResponse = ":Err:NullResponse:";
             }
             else{
-                System.out.println(":Response:");
                 inputStreamFromAPIResponse = similarMoviesHTTPResponse.getEntity().getContent();
-                System.out.println(":Response:1:");
                 if ("gzip".equals(similarMoviesHTTPResponse.getEntity().getContentEncoding())){
                     inputStreamFromAPIResponse = new GZIPInputStream(inputStreamFromAPIResponse);
-                    System.out.println(":Encoding:"+similarMoviesHTTPResponse.getEntity().getContentEncoding().toString());
                 }
 
-                System.out.println(":Response:2:");
+                apiResponseInputStreamReader = new InputStreamReader(inputStreamFromAPIResponse);
+                inputStreamBufferedReader = new BufferedReader(apiResponseInputStreamReader);
 
-                InputStreamReader apiResponseInputStreamReader = new InputStreamReader(inputStreamFromAPIResponse);
                 StringBuilder inputStreamStringBuilder = new StringBuilder();
-                BufferedReader inputStreamBufferedReader = new BufferedReader(apiResponseInputStreamReader);
                 String inputRead = inputStreamBufferedReader.readLine();
 
                 while(inputRead != null) {
-                    System.out.println(":inputRead:"+inputRead);
                     inputStreamStringBuilder.append(inputRead);
                     inputRead = inputStreamBufferedReader.readLine();
                 }
@@ -232,9 +228,13 @@ public class DisplaySimilarMoviesListActivity extends ListActivity{
             System.out.println(":Err:IOException:");
         } catch (Exception e) {
             errorMessage.concat(":Err:Exception:");
-            System.out.println(":Err:Exception:"+e);
+            System.out.println(":Err:Exception:");
         }
         finally {
+            IOUtils.closeQuietly(inputStreamBufferedReader);
+            IOUtils.closeQuietly(inputStreamFromAPIResponse);
+            IOUtils.closeQuietly(apiResponseInputStreamReader);
+
             return apiResponse;
         }
     }
